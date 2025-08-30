a5251f548994dd1582a6b0b20dead835dc9dde8b
Lemon Pecan Pie <aidanhpaul@gmail.com>
2025-08-30 08:03:14 -0400

    Write delay function

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 for (let n = 2; n <= 10; n += 2) {
     alert(n);
 }
+
+function delay(ms) {
+    return new Promise(function
+        (resolve, reject) {
+            setTimeout(resolve, ms);
+        }
+    )
+}
